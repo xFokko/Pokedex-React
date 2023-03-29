becaafaabe066d90939960349c8fb55c18d038cd
axel castro <castroaxel.f@gmail.com>
2023-03-29 15:25:11 +0200

    props done

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
-const App=() =>{
+
+const App = () => {
+  const pokemonList = [
+    {
+      name: "BULBASAUR",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "MEW",
+    },
+  ];
+
+  const pokemon = pokemonList[0];
+
   return (
     <div>
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
-}
+};
+
+
 
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,29 +1,11 @@
-const PokemonCard=()=>{
-    // return <figure    className="card">
-    
-    //   <img
-    //  className="card_img"
-    //      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-    //      alt="Bulbasaur" />
-    //  <figcaption className="cardText">BULBASAUR</figcaption>
-    //  </figure> 
-
-    const pokemon = pokemonList[0] 
+const PokemonCard=({ pokemon })=>{
+ 
+ 
     return (
     <figure className="card"> {pokemon.imgSrc ? <img className="card_img" src={pokemon.imgSrc} alt={pokemon.name}/> : <p>"???"</p>}
     <figcaption className="cardText">{pokemon.name}</figcaption>
     </figure>)
     }
 
-const pokemonList = [
-    {
-      name: "BULBASAUR",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "MEW",
-    },
-  ];
 
 export default PokemonCard
