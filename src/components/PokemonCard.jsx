35e958cedf37d849ea7ef307d3ce756fa2d1972f
axel castro <castroaxel.f@gmail.com>
2023-03-29 11:24:26 +0200

    affichage dynamique des cards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,29 @@
 const PokemonCard=()=>{
-    return <figure    className="card">
+    // return <figure    className="card">
     
-     <img
-     className="card_img"
-         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="Bulbasaur" />
-     <figcaption className="cardText">BULBASAUR</figcaption>
-     </figure>}
+    //   <img
+    //  className="card_img"
+    //      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    //      alt="Bulbasaur" />
+    //  <figcaption className="cardText">BULBASAUR</figcaption>
+    //  </figure> 
+
+    const pokemon = pokemonList[0] 
+    return (
+    <figure className="card_img"> {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>"???"</p>}
+    <figcaption>{pokemon.name}</figcaption>
+    </figure>)
+    }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCard
